de03f625cfd48e3ebf828e6ddff2f9e69dc68787
Jonatas Braz de Sousa <jonatas.sousa@cofen.gov.br>
2019-01-18 09:54:23 -0200

    typescript definitions, declare var,

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -1,37 +1,37 @@
 class NegociacaoController {
 
-    private _inputData: HTMLInputElement;
-    private _inputQuantidade: HTMLInputElement;
-    private _inputValor: HTMLInputElement;
+    private _inputData: JQuery;
+    private _inputQuantidade: JQuery;
+    private _inputValor: JQuery;
     //pode deixar apenas abaixo, mas eu gostei de especificar o tipo
     //private _negociacaoes = new Negociacoes();
-    private _negociacaoes: Negociacoes = new Negociacoes();
+    private _negociacoes: Negociacoes = new Negociacoes();
 
     private _negociacoesView = new NegociacoesView('#negociacoesView');
     private _mensagemView = new MensagemView('#mensagemView');
 
     constructor() {
-        this._inputData = <HTMLInputElement>document.querySelector('#data');
-        this._inputQuantidade = <HTMLInputElement>document.querySelector('#quantidade');
-        this._inputValor = <HTMLInputElement>document.querySelector('#valor');
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
 
         // atualiza a view para exibir os dados do modelo, vazio
-        this._negociacoesView.update(this._negociacaoes);
+        this._negociacoesView.update(this._negociacoes);
     }
     
     adiciona(event: Event) {
         event.preventDefault();
 
         const negociacao = new Negociacao(
-            new Date(this._inputData.value.replace(/-/g,',')),
-            parseInt(this._inputQuantidade.value),
-            parseFloat(this._inputValor.value)
+            new Date(this._inputData.val().replace(/-/g,',')),
+            parseInt(this._inputQuantidade.val()),
+            parseFloat(this._inputValor.val())
         )
 
-        this._negociacaoes.adiciona(negociacao);
+        this._negociacoes.adiciona(negociacao);
 
         // depois de adicionar, atualiza a view novamente para refletir os dados
-        this._negociacoesView.update(this._negociacaoes);
+        this._negociacoesView.update(this._negociacoes);
         this._mensagemView.update('Negociação adicionada com sucesso.');
 
 
